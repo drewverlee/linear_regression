33979ebdc7b3faa30e1ebae4bbeaeb778cefb39b
Drew Verlee <drew.verlee@atomicobject.com>
2015-02-18 18:23:59 -0500

    pre use in post

--- linear_regression.py
+++ linear_regression.py
@@ -31,13 +31,16 @@ class PlotContainer(object):
         for i, plot in enumerate(self.plots[start: stop]):
             plot(self.dfs[i])
 
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1), scatterpoints=1, ncol=4,
+                fancybox=True, shadow=True, prop={'size':10}, markerscale=1 )
         title = 'linear_regression_{0}_{1}'.format(df.name, i)
         plt.title(title)
-        plt.xlabel('x')
-        plt.ylabel('y')
+        plt.xlabel('x points')
+        plt.ylabel('y points')
         plt.grid()
         plt.gca().set_aspect('equal', adjustable='box')
         plt.savefig(title)
+        plt.gca().cla()
 
     def successive(self):
         """
@@ -55,6 +58,15 @@ def scatter_plot(df):
     plt.scatter(x=df.x, y=df.y, color='g', marker='x', 
             linewidth=20, alpha=0.8, label='data points')
 
+def estimated_y_plot(df):
+    """
+    df: DataFrame
+    creates a 2d line that represents the estimated y points.
+    """
+    plt.plot(df.x, df.estimated_y, 'bo', 
+            color="red", linewidth=5, alpha=0.8, label='estimated y', 
+            marker='x', markersize=15)
+
 
 def regression_line_plot(df):
     """
@@ -62,10 +74,8 @@ def regression_line_plot(df):
     creates a 2d line that represents the regression line through the x and y 
     points.
     """
-    plt.plot(df.x, regression_line(df.x, df.slope, df.intercept) , 'bo', 
-            df.x,  regression_line(df.x, df.slope, df.intercept), 'k', 
-            color="black", c=10, linewidth=5, alpha=0.8,
-            marker='x', markersize=15)
+    plt.plot(df.x, regression_line(df.x, df.slope, df.intercept) , 
+            color="red", linewidth=5, alpha=0.6, label='regression line')
 
 
 def sum_of_squares_regression_plot_with_line(df):
@@ -79,8 +89,11 @@ def sum_of_squares_regression_plot_with_line(df):
         #  between (x,y) and (x, estimated)
         x = (df.x[i], df.x[i])
         y = (df.estimated_y[i], df.y[i])
-        
-        plt.plot(x, y, color='blue', linewidth=5, alpha=0.6, linestyle='--')
+        if i == 0:
+            plt.plot(x, y, 
+                    color='blue', linewidth=5, alpha=0.6, linestyle='--', label='least squares')
+        else:
+            plt.plot(x, y, color='blue', linewidth=5, alpha=0.6, linestyle='--')
 
 def sum_of_squares_regression_plot_with_box(df):
     """
@@ -105,8 +118,11 @@ def sum_of_squares_regression_plot_with_box(df):
         ax = (df.x[i] + d, df.x[i] + d)
         ay = (df.y[i], df.estimated_y[i])
 
-        plt.plot(x, y, tx, ty, bx, by, ax, ay, 
-                color='silver', linewidth=5, alpha=0.6, linestyle='--')
+        if i == 0:
+            plt.plot(x, y, tx, ty, bx, by, ax, ay, 
+                    color='silver', linewidth=5, alpha=0.6, linestyle='--')
+        else:
+            plt.plot(x, y, tx, ty, bx, by, ax, ay, color='silver', linewidth=5, alpha=0.6, linestyle='--')
 
 
 def regression_line(x, slope, intercept): 
@@ -137,24 +153,24 @@ def linear_regression(df):
     """
     df.estimated_y = estimated_y(df)
     plots = [scatter_plot, sum_of_squares_regression_plot_with_line, 
-            sum_of_squares_regression_plot_with_box, regression_line_plot]
+            sum_of_squares_regression_plot_with_box, estimated_y_plot, regression_line_plot]
     dfs = [df] * len(plots)
     dfs_plot_container = PlotContainer(plots, dfs)
     dfs_plot_container.successive()
 
 if __name__ == '__main__':
 
-    rec = pd.DataFrame({
+    square = pd.DataFrame({
         'x': [1,1,5,5,10,10],
         'y': [1,5,1,5,1,5]})
-    rec.name = 'square'
+    square.name = 'square'
 
     outlier = pd.DataFrame({
         'x': [1,1,5,5,10,10],
         'y': [1,5,1,10,30,5]})
     outlier.name = 'outlier'
 
-    data = [rec, outlier]
+    data = [square, outlier]
 
     for df in data:
         linear_regression(df)
